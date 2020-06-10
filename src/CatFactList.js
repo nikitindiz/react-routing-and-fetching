4e4b9e02645a9a2b42943a9583e82218d498ce66
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-10 10:55:19 +0300

    Cat fact list initial state added

--- src/CatFactList.js
+++ src/CatFactList.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class CatFactList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: 'IDLE', // STARTED, SUCCEED, FAILED
+            errorMessage: null,
+            catFact: [],
+        };
+    }
+
     render() {
         return (
             <div>
