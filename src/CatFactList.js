511050a661c7b686f55c31ad407194b5f632d63c
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-10 11:09:01 +0300

    Basic fetching sequence implemented

--- src/CatFactList.js
+++ src/CatFactList.js
@@ -7,16 +7,65 @@ class CatFactList extends React.Component {
         this.state = {
             status: 'IDLE', // STARTED, SUCCEED, FAILED
             errorMessage: null,
-            catFact: [],
+            catFacts: [],
         };
     }
 
     render() {
+        const { status, catFacts } = this.state;
+
         return (
             <div>
-                Cat fact list
+                {status === 'IDLE' && (
+                    <div>
+                        Component has being mount
+                        and will start fetching in a moment
+                    </div>
+                )}
+
+                {status === 'STARTED' && (
+                    <div>
+                        Loading...
+                    </div>
+                )}
+
+                {status === 'SUCCEED' && (
+                    <div>
+                        { catFacts.length } facts downloaded
+                    </div>
+                )}
+
+                {status === 'FAILED' && (
+                    <div style={{ backgroundColor: 'red' }}>
+                        Cat facts fetching failed
+                    </div>
+                )}
             </div>
         )
+    }
+
+    componentDidMount() {
+        this.setState({
+            status: 'STARTED',
+            errorMessage: null
+        }, () => {
+            fetch('https://cat-fact.herokuapp.com/facts')
+                .then(response => {
+                    return response.json();
+                })
+                .then(catFactsList => {
+                    this.setState({
+                        catFacts: catFactsList.all,
+                        status: 'SUCCEED'
+                    });
+                })
+                .catch((error) => {
+                    this.setState({
+                        status: 'FAILED',
+                        errorMessage: error.message,
+                    })
+                });
+        })
     }
 }
 
