809f144b0e2b70ea647ac013ddc538e2e6a621a6
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-10 10:33:55 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="app-header">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cat-facts">Cat facts</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+const AboutPage = () => {
+  return (
+    <div>
+      About page
+    </div>
+  );
+}
+
+const CatFactsPage = () => {
+  return (
+    <div>
+      Cat facts page
+    </div>
+  );
+}
+
+const HomePage = () => {
+  return (
+    <div>
+      Home page
+    </div>
+  );
+}
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/about">
+        <AboutPage />
+      </Route>
+      <Route path="/cat-facts">
+        <CatFactsPage />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Routes />
     </div>
   );
 }
