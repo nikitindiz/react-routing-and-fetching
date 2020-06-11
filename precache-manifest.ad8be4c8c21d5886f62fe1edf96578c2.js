d18fdde46c4efb44bacddad8de9fe2447effbeb2
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-11 15:30:47 +0300

    Add gh-pages branch

---
+++ precache-manifest.ad8be4c8c21d5886f62fe1edf96578c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b124839cea67fcfe02b8e2e44c1a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db8450d370686f4229fe",
+    "url": "/static/css/main.09df10ff.chunk.css"
+  },
+  {
+    "revision": "f0eedd9abc8dfb827984",
+    "url": "/static/js/2.dd5f96c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dd5f96c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8450d370686f4229fe",
+    "url": "/static/js/main.f576b97e.chunk.js"
+  },
+  {
+    "revision": "8561db8a19f1aeda3b59",
+    "url": "/static/js/runtime-main.c303b79c.js"
+  }
+]);
